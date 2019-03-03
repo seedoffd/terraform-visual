adaa149d79e3854454b063b0e7cd1217881cf1c0
seedoffd <seedoffd@gmail.com>
2019-03-03 16:11:13 -0600

    instance.tf

--- instance.tf
+++
@@ -1,7 +0,0 @@
-resource "aws_instance" "example" {
-    ami = "${var.ami}"
-    instance_type = "${var.instance_type}"
-    key_name = "${var.subnet_id}" 
-    subnet_id = "${var.subnet_id}"
-    associate_public_ip_address = true
-}
